85f5deb85dfd640c7d173c6222bcd11d03ba88de
sebastiano minotti <seba.minotti@gmail.com>
2021-04-28 12:21:43 +0200

    esecuzione snack 6

---
+++ snack5/main.js
@@ -0,0 +1,22 @@
+
+// numeroUtente= Math.pow(2,2);
+// console.log(numeroUtente);
+
+
+
+numero = Number(prompt("numero inserito"));
+
+numero = Math.pow(numero ,3)
+console.log(numero)
+
+
+
+
+
+
+for(var i =0; i< 5;i++){
+    numero = Number(prompt("numero inserito"));
+
+    numero = Math.pow(numero ,3)
+    console.log(numero)
+}
