377ec8438f734af63ecd1e3b9b78cb72801320e8
sebastiano minotti <seba.minotti@gmail.com>
2021-05-10 11:48:10 +0200

    snack2b

--- jSnack 1a/main.js
+++ jSnack 1a/main.js
@@ -6,3 +6,11 @@ peso : "10 kg"
 }
 
 //------------------------------------------------------------------------------------
+
+// palla["prezzo"] = 70;
+// console.log(palla.prezzo);
+var nuovoPeso =prompt("aggiungi peso alla palla")
+
+palla ["peso"]= nuovoPeso
+
+console.log(palla);
